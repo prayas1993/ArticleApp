d1190b65ea5a627e6f5c8fb3d93ace6483018d1e
HEY <you@example.com>
2017-08-02 20:45:16 +0530

    Add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def create
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id]);
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
