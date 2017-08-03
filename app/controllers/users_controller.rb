d51f6eb0c6980f842bebae87321997e958e85ea3
HEY <you@example.com>
2017-08-03 08:46:09 +0530

    Helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:edit, :update, :show]
+
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
-    @users = User.paginate(page: params[:page], per_page: 5)
+    @user=User.new
   end
 
   def create
@@ -18,16 +21,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id]);
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
-    @user = User.find(params[:id]);
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Account Updated Successfully"
       redirect_to articles_path
@@ -46,6 +46,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username , :email , :password)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def require_current_user
     if current_user != User.find(current_user.id)
@@ -54,3 +58,4 @@ class UsersController < ApplicationController
     end
   end
 end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  
+ 
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+ 
   resources :articles
 
   root 'page#home'
