9ebb2a5d6458fb95b5f33c65e2a4dc54119c56e9
HEY <you@example.com>
2017-08-03 11:02:11 +0530

    Random Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    session[:user_id] = @user.id
     if @user.save
       flash[:success] = "welcome to my WebApp #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
